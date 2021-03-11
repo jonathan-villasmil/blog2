4d3e2c522737e9a2daaa92732108af8fbdcbbe74
jonathan villasmil <jonathanvillasmildeabreu@gmail.com>
2021-03-11 11:03:11 +0100

    config templates

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-bold text-xl text-blue-800 leading-tight">
+            BonsaiCO
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quod? Molestias, corporis, beatae deleniti, magnam error commodi repellat ullam iure praesentium quis quod modi eveniet aut natus voluptatem quas omnis.</p>
+                <p>Facilis quasi autem iure vitae. Quidem, ducimus voluptatem molestias sed aliquam asperiores eligendi consectetur adipisci deserunt neque id est vitae possimus aliquid non aut. Autem corrupti animi asperiores quaerat quae.</p>
+                <p>Error hic similique obcaecati repudiandae assumenda laudantium nihil velit aut sequi suscipit officia vitae porro ea provident fugiat, officiis aliquid voluptate praesentium exercitationem sint in facilis distinctio, odit dolores. Deleniti!</p>
+                <p>Rerum autem accusantium omnis vitae neque perspiciatis repellendus. Totam ab eius in, commodi nesciunt rerum consequuntur eum quibusdam. Ex nostrum totam placeat temporibus ratione. Natus id labore explicabo nostrum praesentium.</p>
+                <p>Molestiae natus dolore expedita! Aspernatur, recusandae! Dignissimos id, consequuntur nemo numquam magni fugit ab ipsa animi necessitatibus, veritatis magnam totam ducimus libero eum nihil ipsam! Architecto accusamus quibusdam omnis assumenda.</p>
             </div>
         </div>
     </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,10 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+
+                    <x-jet-nav-link :active="request()->routeIs('blog')" href="">
+                        blog
+                    </x-jet-nav-link>
                 </div>
             </div>
 
@@ -140,6 +144,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+
+            <x-jet-responsive-nav-link href="" :active="request()->routeIs('blog')">
+                Blog
             </x-jet-responsive-nav-link>
         </div>
 
